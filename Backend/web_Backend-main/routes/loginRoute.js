c66194a65feceeb7c50b446d35c10a1f029fef23
unknown <ahmedmokhtar2407@gmail.com>
2024-05-15 23:23:43 +0300

    finall

--- Backend/web_Backend-main/routes/loginRoute.js
+++ Backend/web_Backend-main/routes/loginRoute.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const routes = express.Router();
-const {logIn, getLoginByUsername,signup} = require('../controllers/login')
-routes.route("/login").post(signup)
+const {logIn, getLoginByUsername,signup,getAllLogin} = require('../controllers/login')
+routes.route("/login").post(signup).get(getAllLogin)
 routes.route("/login:username").post(logIn)
 
 module.exports = routes;
